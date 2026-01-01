e5321aee24dc08cae49b88c44e4465284e21e02a
NahidHasan131 <nahid7532140@gmail.com>
2026-01-02 00:43:21 +0600

    visited country count from country api

--- world-countreis-info/src/Components/Countries/Countries.jsx
+++ world-countreis-info/src/Components/Countries/Countries.jsx
@@ -1,15 +1,43 @@
-import React, { use } from 'react';
+import React, { use, useState } from 'react';
 import Country from '../Country/Country';
+import './Countries.css';
 
 const Countries = ({ promiseCountries }) => {
+
+    const[visitedCount, setVisitedCount] = useState([]);
+    const handleVisitedCountry = (country) => {
+        const  newVisitedCount = [...visitedCount, country];
+        setVisitedCount(newVisitedCount);
+    }
+
+
+    // const[visitedCount, setVisitedCount] = useState(0);
+
+    // const handleVisitedCountry = () => {
+    //     setVisitedCount(visitedCount + 1);
+    // }
+    // const handleNotVisitedCountry = ()=>{
+    //     setVisitedCount(visitedCount - 1);
+    // }
+
     const countries = use(promiseCountries);
-    console.log(countries);
+    // console.log(countries);
     return (
         <div>
-            <h1>Travelling Countries: {countries.length}</h1>
-            {
-             countries.map(country => <Country key={country.name.common} country={country}></Country>)
-            }
+            <h1>Traveling Countries: {countries.length}</h1>
+            {/* <h3>Count visited Country: {visitedCount}</h3> */}
+            <h3>Traveled So  Far: {visitedCount.length}</h3>
+            <ol>
+                {
+                    visitedCount.map(c => <li key={c.name.common}>{c.name.common}</li>)
+                }
+            </ol>
+            <div className='countries'>
+                {
+                    // countries.map(country => <Country key={country.name.common} country={country} onVisited={handleVisitedCountry} notVisited={handleNotVisitedCountry}></Country>)
+                    countries.map(country => <Country key={country.name.common} country={country} onVisited={handleVisitedCountry}></Country>)
+                }
+            </div>
         </div>
         
     );
--- world-countreis-info/src/Components/Country/Country.jsx
+++ world-countreis-info/src/Components/Country/Country.jsx
@@ -1,12 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
+import './Country.css';
 
-const Country = ({ country }) => {
+const Country = ({ country, onVisited}) => {
+
+    const [Visited, setVisited] = useState(false);
+
+    const handleVisited = ()=>{
+        setVisited(!Visited);
+        onVisited(country)
+
+    //    if(Visited === true){
+    //     setVisited(false);
+    //     notVisited();
+    //    }
+    //     else{
+    //         setVisited(true); 
+    //         onVisited();
+    //     }
+    }
     return (
-        <div>
+        <div className={`country ${Visited && 'visited-country'}`}>
             <h2>Name: {country.name.common}</h2>
             <img src={country.flags.png} alt={country.name.common}></img>
-            <p>Independet: {country.independent ? "Free" : "Not Free"}</p>
+            <p>Independent: {country.independent ? "Free" : "Not Free"}</p>
             <p>Population: {country.population}</p>
+            <button onClick={handleVisited}>
+               {Visited ? "Visited" : "Not Visited"}
+            </button>
         </div>
     );
 };
